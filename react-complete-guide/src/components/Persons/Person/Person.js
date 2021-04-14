944ac817c172f6ac06c129d48233c7a40d5383d1
Xuehua Chen <xuehua@gmail.com>
2021-04-14 13:45:09 -0700

    Added ref usage via React.createRef() in component approach in Person.js

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -4,8 +4,16 @@ import PropTyes from 'prop-types';
 import Aux from '../../../hoc/Auxilary'
 
 class Person extends(Component) {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
     componentWillUnmount() {
         console.log('[Person.js] componentWillUnmount');
+    }
+
+    componentDidMount() {
+        this.inputElementRef.current.focus();
     }
 
     render() {
@@ -20,6 +28,7 @@ class Person extends(Component) {
                     type='text' 
                     onChange={this.props.changed} 
                     value={this.props.name} 
+                    ref = {this.inputElementRef}
                 />
             </Aux>
         );
