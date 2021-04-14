0509f33895231838deccc365c83098141baf7a61
Xuehua Chen <xuehua@gmail.com>
2021-04-14 12:25:51 -0700

    Use Aux hoc.

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Person.css';
+import Aux from '../../../hoc/Auxilary'
 
 class Person extends(Component) {
     componentWillUnmount() {
@@ -9,11 +10,17 @@ class Person extends(Component) {
     render() {
         console.log('[Person.js] rendering')
         return (
-            <div className='Person'>
-                <p onClick={this.props.click}> I'm {this.props.name} and I am {this.props.age} years old</p>
+            <Aux>
+                <p onClick={this.props.click}> 
+                    I'm {this.props.name} and I am {this.props.age} years old
+                </p>
                 <p>{this.props.children}</p>
-                <input type='text' onChange={this.props.changed} value={this.props.name} />
-            </div>
+                <input 
+                    type='text' 
+                    onChange={this.props.changed} 
+                    value={this.props.name} 
+                />
+            </Aux>
         );
     }
     
