32594f55357c07b1006466c3a07f9068f9fcec01
Xuehua Chen <xuehua@gmail.com>
2021-04-16 13:19:14 -0700

    Fixed typos.

--- react-guide/src/components/NewExpense/ExpenseForm.js
+++ react-guide/src/components/NewExpense/ExpenseForm.js
@@ -6,20 +6,20 @@ const ExpenseForm = () => {
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
-          <lable>Title</lable>
+          <label>Title</label>
           <input type="text" />
         </div>
         <div className="new-expense__control">
-          <lable>Amount</lable>
+          <label>Amount</label>
           <input type="number" min="0.01" step="0.01" />
         </div>
         <div className="new-expense__control">
-          <lable>Amount</lable>
+          <label>Amount</label>
           <input type="date" min="2019-01-01" max="2022-12-31" />
         </div>
-        <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
-        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
