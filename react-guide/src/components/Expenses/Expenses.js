40219345b4fdf76bab587ed7cebc4be965d00bf8
Xuehua Chen <xuehua@gmail.com>
2021-04-19 13:05:39 -0700

    Display expenses filtered by year.

--- react-guide/src/components/Expenses/Expenses.js
+++ react-guide/src/components/Expenses/Expenses.js
@@ -7,18 +7,22 @@ import ExpensesFilter from "./ExpensesFilter";
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
 
+  const filteredItems = props.items.filter(
+    (item) => item.date.getFullYear().toString() === filteredYear
+  );
   const filterChangedHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
     console.log("Selected year is ", selectedYear);
   };
 
+  // .filter((item) => item.date.getYear() === filteredYear - 1900)
   return (
     <Card className="expenses">
       <ExpensesFilter
         selectedYear={filteredYear}
         onFilterChange={filterChangedHandler}
       />
-      {props.items.map((item) => {
+      {filteredItems.map((item) => {
         return (
           <ExpenseItem
             key={item.id}
