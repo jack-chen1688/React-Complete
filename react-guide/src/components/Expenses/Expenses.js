71f22541d333f8eed82936cae823fe9240104afd
Xuehua Chen <xuehua@gmail.com>
2021-04-21 12:10:14 -0700

    Added chart bar into the project.

--- react-guide/src/components/Expenses/Expenses.js
+++ react-guide/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -25,6 +26,7 @@ function Expenses(props) {
         selectedYear={filteredYear}
         onFilterChange={filterChangedHandler}
       />
+      <ExpensesChart expenses={filteredItems} />
       <ExpensesList items={filteredItems} />
     </Card>
   );
