b7ed56f84e17b490e47c2ac6892cf300edb10356
Xuehua Chen <xuehua@gmail.com>
2021-05-01 11:03:21 -0700

    Complete the Header component.

--- food-order/src/components/Layout/Header.js
+++ food-order/src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React, { Fragment } from "react";
 
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCardButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1> ReactMeals</h1>
-        <h1>Cart</h1>
+        <HeaderCardButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
